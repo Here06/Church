fb76db9e2836ea419f287fce0dda174e0c2fcb5d
uhone.nndwamato <uhone1593@gmail.com>
2025-09-06 10:04:54 +0200

    unused import

--- src/app/components/branch-card/branch-card.component.ts
+++ src/app/components/branch-card/branch-card.component.ts
@@ -1,6 +1,5 @@
 import { Router } from '@angular/router';
 import {Component, Input} from "@angular/core";
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-branch-card',
