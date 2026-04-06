d309f17146ffa6e1fdf1dc007fd91ea69ca7ccca
uhone.nndwamato <uhone1593@gmail.com>
2026-04-06 22:36:55 +0200

    animations

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {CommonModule, isPlatformBrowser} from '@angular/common';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 interface TimelineEvent {
   year: string;
@@ -14,18 +16,15 @@ interface TimelineEvent {
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent {
+
+export class HistoryComponent implements OnInit {
   timeline: TimelineEvent[] = [
     {
-      year: '1985',
+      year: '1937',
       title: 'Founding of NCCC',
       description: 'Our church was established with a small group of believers...'
     },
-    {
-      year: '1995',
-      title: 'First Branch Opened',
-      description: 'We expanded to serve a wider community...'
-    },
+    {year: '1995', title: 'First Branch Opened', description: 'We expanded to serve a wider community...'},
     {
       year: '2010',
       title: 'Community Outreach Programs',
@@ -37,4 +36,16 @@ export class HistoryComponent {
       description: 'Started livestream services and online fellowship during the pandemic...'
     }
   ];
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+        duration: 600,
+        once: true
+      });
+    }
+  }
 }
